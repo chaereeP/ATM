c710fc249d1452ca3a9660da1b18bfded085f183
cherry <49869873+chaereeP@users.noreply.github.com>
2024-12-05 04:44:03 +0900

    test/ pass msg

--- ATM/test_main.cpp
+++ ATM/test_main.cpp
@@ -8,16 +8,16 @@
 
 
 int main() {
-	std::cout << "Starting tests...\n\n\n";
+	std::cout << "========== Starting Test Suite ==========\n\n";
 
 	// Run all the tests for Account
-	std::cout << "Running Account tests...\n";
+	std::cout << "[TEST]Running Account tests...\n";
 	int accountTestsResult = testAccount();
 
 	// Run all the tests for ATMController
-	std::cout << "Running ATMController tests...\n";
+	std::cout << "[TEST] Running ATMController tests...\n";
 	int atmTestsResult = testATMController();
 
-	std::cout << "All tests completed successfully.\n";
+	std::cout << "[PASS] All tests completed successfully.\n";
 	return 0;
 }
